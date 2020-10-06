e9d2a9ab17d8fa3ce284c88eb034ff8b340d0761
Daniel de Sousa Oliveira Melo Veras <daniel_sousa.unb@outlook.com>
2020-10-06 10:44:18 -0300

    :alien: Updating server configuration
    Co-authored-by: Lucas Leite <lucasleitemm@hotmail.com>
    Co-authored-by: Lucas Medeiros <medeirosrosalucas@gmail.com>

--- sya/src/shared/infra/http/server.ts
+++ sya/src/shared/infra/http/server.ts
@@ -1,16 +1,37 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 import 'express-async-errors';
+
+import AppError from '@shared/errors/AppError';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.use(errors());
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'erorr',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internet server error',
+  });
+});
+
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333');
+  console.log('⏱ Server started on port 3333');
 });
