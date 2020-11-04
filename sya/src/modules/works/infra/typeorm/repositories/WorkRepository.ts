abca1da9d97675fe1eb04a72029616e178e27d2e
Lucas Leite <lucasleitemm@hotmail.com>
2020-11-04 01:23:48 -0300

    Create Work Interface and implementation

---
+++ sya/src/modules/works/infra/typeorm/repositories/WorkRepository.ts
@@ -0,0 +1,25 @@
+import ICreateWorkDTO from '@modules/works/dtos/ICreateWorkDTO';
+import IWorkRepository from '@modules/works/repositories/IWorkRepository';
+import { getRepository, Repository } from 'typeorm';
+import Work from '../entities/Work';
+
+class WorkRepository implements IWorkRepository {
+  private ormRepository: Repository<Work>;
+
+  constructor() {
+    this.ormRepository = getRepository(Work);
+  }
+
+  public findById(id: string): Promise<Work | undefined> {
+    const findWork = this.ormRepository.findOne(id);
+    return findWork;
+  }
+
+  public async create(workData: ICreateWorkDTO): Promise<Work> {
+    const work = this.ormRepository.create(workData);
+    await this.ormRepository.save(work);
+    return work;
+  }
+}
+
+export default WorkRepository;
