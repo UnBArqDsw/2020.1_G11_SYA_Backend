8e6fcda21694e0ce5a10436383915c3b2511748c
Lucas Leite <lucasleitemm@hotmail.com>
2020-11-04 02:00:54 -0300

    Create work controller and route '/'

---
+++ sya/src/modules/works/infra/http/routes/works.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
+import WorksController from '@modules/works/infra/http/controllers/WorksController';
+import ensureAuthenticated from '@shared/infra/http/middlewares/ensureAuthenticated';
+
+const workRouter = Router();
+
+const workController = new WorksController();
+workRouter.use(ensureAuthenticated);
+workRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      price: Joi.number().required(),
+      duration: Joi.string().required(),
+    },
+  }),
+  workController.create
+);
+
+export default workRouter;
--- sya/src/shared/infra/http/routes/index.ts
+++ sya/src/shared/infra/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import userRouter from '@modules/users/infra/http/routes/users.routes';
 import employeesRouter from '@modules/employees/infra/http/routes/employees.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import worksRouter from '@modules/works/infra/http/routes/works.routes';
 
 const routes = Router();
 
 routes.use('/users', userRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/employees', employeesRouter);
+routes.use('/works', worksRouter);
 
 export default routes;
